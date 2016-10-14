8b905173fe3b65a3241f4b2bb4debd8f46ad1e46
Lei Xu <leixu216@gmail.com>
2016-10-14 12:49:18 +0800

    code

---
+++ CalculatorDemo.Tests/CalculatorTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorDemo;
+
+namespace CalculatorDemo.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Calculator_Add_Input2And2ShouldReture4_Success()
+        {
+            Calculator sut = new Calculator();
+            int expected = 4;
+            int num1 = 2;
+            int num2 = 2;
+            int result = sut.Add(num1, num2);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Calculator_Mul_Input2And2ShouldReture4_Success()
+        {
+            Calculator sut = new Calculator();
+            int exepcted = 4;
+            int num1 = 2;
+            int num2 = 2;
+            int result = sut.Mul(num1, num2);
+            Assert.AreEqual(exepcted, result);
+        }
+
+        [TestMethod]
+        public void Calculator_Mul_Input2And3ShouldReture6_Success()
+        {
+            Calculator sut = new Calculator();
+            int exepcted = 6;
+            int num1 = 2;
+            int num2 = 3;
+            int result = sut.Mul(num1, num2);
+            Assert.AreEqual(exepcted, result);
+        }
+
+        [TestMethod]
+        public void Calculator_Divid_Input6And2ShouldReture3_Success()
+        {
+            Calculator sut = new Calculator();
+            int exepected = 3;
+            int num1 = 6;
+            int num2 = 2;
+            int result = sut.Divid(num1, num2);
+            Assert.AreEqual(exepected, result);
+        }
+
+        [TestMethod]
+        public void Calcualtor_IsNativePrivete_InputNagtiveNumber_ShouldBeTrue()
+        {
+            PrivateObject sut = new PrivateObject(typeof(Calculator));
+            bool result = (bool)sut.Invoke("IsNagtive", -1);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ CalculatorDemo/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDemo
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Divid(int num1, int num2)
+        {
+            if (IsNagtive(num1) || IsNagtive(num2))
+            {
+                throw new Exception("number cannnot be nagtive!");
+            }
+            return num1 / num2;
+        }
+
+        private bool IsNagtive(int num2)
+        {
+            if (num2<0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
